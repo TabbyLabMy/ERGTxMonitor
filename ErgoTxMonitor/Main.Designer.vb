25a9de5349d27d84792f7e70a991dbd17a137176
lkching7 <134677998+TabbyPOS@users.noreply.github.com>
2023-06-29 01:41:23 +0800

    First

---
+++ ErgoTxMonitor/Main.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.btnGetTx = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnGetHeight = New System.Windows.Forms.Button()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.GridControl1 = New DevExpress.XtraGrid.GridControl()
+        Me.GridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+        Me.GridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn4 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn5 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.GridColumn6 = New DevExpress.XtraGrid.Columns.GridColumn()
+        Me.imgGreen = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.LabelControl1 = New DevExpress.XtraEditors.LabelControl()
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.imgGreen, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnGetTx
+        '
+        Me.btnGetTx.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGetTx.Location = New System.Drawing.Point(822, 110)
+        Me.btnGetTx.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnGetTx.Name = "btnGetTx"
+        Me.btnGetTx.Size = New System.Drawing.Size(100, 28)
+        Me.btnGetTx.TabIndex = 0
+        Me.btnGetTx.Text = "Get Tx"
+        Me.btnGetTx.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.Location = New System.Drawing.Point(822, 223)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(316, 256)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 2500
+        '
+        'btnGetHeight
+        '
+        Me.btnGetHeight.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGetHeight.Location = New System.Drawing.Point(930, 109)
+        Me.btnGetHeight.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnGetHeight.Name = "btnGetHeight"
+        Me.btnGetHeight.Size = New System.Drawing.Size(100, 28)
+        Me.btnGetHeight.TabIndex = 2
+        Me.btnGetHeight.Text = "Get Height"
+        Me.btnGetHeight.UseVisualStyleBackColor = True
+        '
+        'btnTest
+        '
+        Me.btnTest.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnTest.Location = New System.Drawing.Point(1038, 109)
+        Me.btnTest.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(100, 28)
+        Me.btnTest.TabIndex = 3
+        Me.btnTest.Text = "Test"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'GridControl1
+        '
+        Me.GridControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GridControl1.Location = New System.Drawing.Point(12, 45)
+        Me.GridControl1.MainView = Me.GridView1
+        Me.GridControl1.Name = "GridControl1"
+        Me.GridControl1.Size = New System.Drawing.Size(1127, 435)
+        Me.GridControl1.TabIndex = 4
+        Me.GridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.GridView1})
+        '
+        'GridView1
+        '
+        Me.GridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.GridColumn1, Me.GridColumn4, Me.GridColumn2, Me.GridColumn3, Me.GridColumn5, Me.GridColumn6})
+        Me.GridView1.GridControl = Me.GridControl1
+        Me.GridView1.Name = "GridView1"
+        Me.GridView1.OptionsView.ShowGroupPanel = False
+        '
+        'GridColumn1
+        '
+        Me.GridColumn1.Caption = "TxID"
+        Me.GridColumn1.FieldName = "TxID"
+        Me.GridColumn1.Name = "GridColumn1"
+        Me.GridColumn1.OptionsColumn.AllowEdit = False
+        Me.GridColumn1.Visible = True
+        Me.GridColumn1.VisibleIndex = 0
+        Me.GridColumn1.Width = 264
+        '
+        'GridColumn4
+        '
+        Me.GridColumn4.Caption = "BoxID"
+        Me.GridColumn4.FieldName = "BoxID"
+        Me.GridColumn4.Name = "GridColumn4"
+        Me.GridColumn4.OptionsColumn.AllowEdit = False
+        Me.GridColumn4.Visible = True
+        Me.GridColumn4.VisibleIndex = 1
+        Me.GridColumn4.Width = 264
+        '
+        'GridColumn2
+        '
+        Me.GridColumn2.Caption = "Address"
+        Me.GridColumn2.FieldName = "WalletAddress"
+        Me.GridColumn2.Name = "GridColumn2"
+        Me.GridColumn2.OptionsColumn.AllowEdit = False
+        Me.GridColumn2.Visible = True
+        Me.GridColumn2.VisibleIndex = 2
+        Me.GridColumn2.Width = 266
+        '
+        'GridColumn3
+        '
+        Me.GridColumn3.Caption = "Value"
+        Me.GridColumn3.FieldName = "Amount"
+        Me.GridColumn3.Name = "GridColumn3"
+        Me.GridColumn3.OptionsColumn.AllowEdit = False
+        Me.GridColumn3.OptionsColumn.FixedWidth = True
+        Me.GridColumn3.Visible = True
+        Me.GridColumn3.VisibleIndex = 3
+        Me.GridColumn3.Width = 120
+        '
+        'GridColumn5
+        '
+        Me.GridColumn5.Caption = "Confirmed"
+        Me.GridColumn5.FieldName = "Confirmed"
+        Me.GridColumn5.Name = "GridColumn5"
+        Me.GridColumn5.OptionsColumn.AllowEdit = False
+        Me.GridColumn5.OptionsColumn.FixedWidth = True
+        Me.GridColumn5.Visible = True
+        Me.GridColumn5.VisibleIndex = 4
+        Me.GridColumn5.Width = 120
+        '
+        'GridColumn6
+        '
+        Me.GridColumn6.Caption = "DateTime"
+        Me.GridColumn6.DisplayFormat.FormatString = "yyyy/MM/dd hh:mm:ss"
+        Me.GridColumn6.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
+        Me.GridColumn6.FieldName = "CreatedDateTime"
+        Me.GridColumn6.Name = "GridColumn6"
+        Me.GridColumn6.OptionsColumn.AllowEdit = False
+        Me.GridColumn6.OptionsColumn.FixedWidth = True
+        Me.GridColumn6.Visible = True
+        Me.GridColumn6.VisibleIndex = 5
+        Me.GridColumn6.Width = 150
+        '
+        'imgGreen
+        '
+        Me.imgGreen.Image = CType(resources.GetObject("imgGreen.Image"), System.Drawing.Image)
+        Me.imgGreen.Location = New System.Drawing.Point(12, 9)
+        Me.imgGreen.Margin = New System.Windows.Forms.Padding(4)
+        Me.imgGreen.Name = "imgGreen"
+        Me.imgGreen.Size = New System.Drawing.Size(27, 25)
+        Me.imgGreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.imgGreen.TabIndex = 6
+        Me.imgGreen.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 9)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(27, 25)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 200
+        '
+        'LabelControl1
+        '
+        Me.LabelControl1.Location = New System.Drawing.Point(47, 15)
+        Me.LabelControl1.Name = "LabelControl1"
+        Me.LabelControl1.Size = New System.Drawing.Size(130, 16)
+        Me.LabelControl1.TabIndex = 8
+        Me.LabelControl1.Text = "Monitor Status : Active"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1151, 492)
+        Me.Controls.Add(Me.LabelControl1)
+        Me.Controls.Add(Me.imgGreen)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GridControl1)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.btnGetHeight)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.btnGetTx)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tabby Deposit Tx Monitor"
+        CType(Me.GridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.GridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.imgGreen, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGetTx As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents btnGetHeight As System.Windows.Forms.Button
+    Friend WithEvents btnTest As System.Windows.Forms.Button
+    Friend WithEvents GridControl1 As DevExpress.XtraGrid.GridControl
+    Friend WithEvents GridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+    Friend WithEvents GridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn4 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn5 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents GridColumn6 As DevExpress.XtraGrid.Columns.GridColumn
+    Friend WithEvents imgGreen As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents LabelControl1 As DevExpress.XtraEditors.LabelControl
+
+End Class
---
+++ ErgoTxMonitor/Main.vb
@@ -0,0 +1,292 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
+Imports System.Data.SqlClient
+
+
+Public Class Main
+
+
+    Dim icount As Integer
+    Dim DBChainHeight, NodeChainHeight As Integer
+    Dim FirstLoad As Boolean = True
+
+    Public Function GetConnStr() As String
+
+        Return Environment.GetEnvironmentVariable("SQLConnStrTabbyPOS")
+
+    End Function
+
+    Public Function GetNodeURL() As String
+
+        Return Environment.GetEnvironmentVariable("ErgoNodeURL")
+
+    End Function
+
+    Public Function GetNodeAPIKey() As String
+
+        Return Environment.GetEnvironmentVariable("NodeAPIKey")
+
+    End Function
+
+    Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RefreshGrid()
+    End Sub
+
+    Private Function INSERT2TxNode( _
+        ByVal ChainHeight As Integer, ByVal TxID As String, ByVal BoxID As String, ByVal WalletAddress As String, _
+        ByVal Amount As Decimal, ByVal Confirmed As Integer) As Boolean
+
+        Dim gCnnPCE As SqlConnection
+        gCnnPCE = New SqlConnection(GetConnStr)
+        Try
+            gCnnPCE.Open()
+            Dim str As String
+            Dim cmd As New SqlCommand(str, gCnnPCE)
+
+            str = "SELECT ID FROM TxNode WHERE TxID = @TxID"
+            cmd.CommandText = str
+            cmd.Parameters.AddWithValue("@TxID", TxID)
+
+            Dim da As New SqlDataAdapter(cmd)
+            Dim dt As New DataTable
+            da.Fill(dt)
+            If dt.Rows.Count > 0 Then
+                str = "UPDATE TxNode " & _
+                     " SET Confirmed = @Confirmed" & _
+                     " WHERE TxID = @TxID"
+
+                cmd.CommandText = str
+                cmd.Parameters.AddWithValue("@ChainHeight", ChainHeight)
+                cmd.Parameters.AddWithValue("@BoxID", BoxID)
+                cmd.Parameters.AddWithValue("@WalletAddress", WalletAddress)
+                cmd.Parameters.AddWithValue("@Amount", Amount)
+                cmd.Parameters.AddWithValue("@Confirmed", Confirmed)
+
+                If cmd.ExecuteNonQuery() > 0 Then
+                    Return True
+                Else
+                    Return False
+                End If
+
+            Else
+                str = "INSERT INTO TxNode(" & _
+                                     "ChainHeight, TxID, BoxID, WalletAddress, Amount, Confirmed) VALUES ( " & _
+                                     "@ChainHeight, @TxID, @BoxID, @WalletAddress, @Amount, @Confirmed) "
+
+                cmd.CommandText = str
+                cmd.Parameters.AddWithValue("@ChainHeight", ChainHeight)
+                cmd.Parameters.AddWithValue("@BoxID", BoxID)
+                cmd.Parameters.AddWithValue("@WalletAddress", WalletAddress)
+                cmd.Parameters.AddWithValue("@Amount", Amount)
+                cmd.Parameters.AddWithValue("@Confirmed", Confirmed)
+
+                If cmd.ExecuteNonQuery() > 0 Then
+                    Return True
+                Else
+                    Return False
+                End If
+
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation)
+        Finally
+            gCnnPCE.Close()
+            gCnnPCE.Dispose()
+        End Try
+
+
+
+    End Function
+
+    Private Function GetChainTx2DB(ByVal MinChainHeight As Integer) As Integer
+
+        Dim strReq As String
+        strReq = GetNodeURL() & "/wallet/transactions?minInclusionHeight=" & MinChainHeight
+        Dim req As WebRequest = WebRequest.Create((strReq))
+        req.Method = "GET"
+        req.Headers("api_key") = GetNodeAPIKey()
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Try
+            Dim resp As HttpWebResponse = CType(req.GetResponse, HttpWebResponse)
+            If (resp.StatusCode = HttpStatusCode.OK) Then
+                Dim dataStream As Stream = resp.GetResponseStream
+                Dim reader As StreamReader = New StreamReader(dataStream)
+                Dim json As String
+                json = reader.ReadToEnd
+                RichTextBox1.Text = json
+
+                Dim data = JsonConvert.DeserializeObject(Of List(Of Object))(json)
+                Dim _ChainHeight As Integer
+                Dim _TxID As String
+                Dim _BoxID As String
+                Dim _WalletAddress As String
+                Dim _Amount As Decimal
+                Dim _Confirmed As Integer
+                Dim count As Integer = 0
+
+                For Each item In data
+                    _ChainHeight = item("outputs")(0)("creationHeight")
+                    _TxID = item("id")
+                    _BoxID = item("outputs")(0)("boxId")
+                    _WalletAddress = item("outputs")(0)("address")
+                    _Amount = item("outputs")(0)("value") / 1000000000
+                    _Confirmed = item("numConfirmations")
+                    count = count + 1
+                    INSERT2TxNode(_ChainHeight, _TxID, _BoxID, _WalletAddress, _Amount, _Confirmed)
+                Next
+
+                If count > 0 Then
+                    Return True
+                Else
+                    Return False
+                End If
+
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation)
+        End Try
+    End Function
+
+ 
+    Private Function GetDBLastHeight()
+        Dim gCnnPCE As SqlConnection
+        gCnnPCE = New SqlConnection(GetConnStr)
+        Try
+
+            gCnnPCE.Open()
+            Dim str As String
+            If FirstLoad = True Then
+                str = "SELECT ISNULL(MAX(ChainHeight),0) FROM TxNode"
+            Else
+                str = "SELECT ISNULL(MAX(ChainHeight),0) FROM TxNode WHERE Confirmed = 0"
+            End If
+
+            Dim cmd As New SqlCommand(str, gCnnPCE)
+            FirstLoad = False
+            Return cmd.ExecuteScalar
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation)
+        Finally
+            gCnnPCE.Close()
+            gCnnPCE.Dispose()
+        End Try
+    End Function
+
+    Private Function GetCurrentChainHeight() As Integer
+        Dim strReq As String
+        strReq = GetNodeURL() & "/blockchain/indexedHeight"
+        Dim req As WebRequest = WebRequest.Create((strReq))
+        req.Method = "GET"
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12
+        Try
+            Dim resp As HttpWebResponse = CType(req.GetResponse, HttpWebResponse)
+            If (resp.StatusCode = HttpStatusCode.OK) Then
+                Dim dataStream As Stream = resp.GetResponseStream
+                Dim reader As StreamReader = New StreamReader(dataStream)
+                Dim Result As String
+                Result = reader.ReadToEnd
+                RichTextBox1.Text = Result
+                Dim json As JObject = JObject.Parse(Result)
+                Return json("fullHeight")
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Function
+
+    Private Sub btnGetHeight_Click(sender As Object, e As EventArgs) Handles btnGetHeight.Click
+        MsgBox(GetCurrentChainHeight, MsgBoxStyle.Information)
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Timer1.Enabled = False
+
+        Try
+            DBChainHeight = GetDBLastHeight()
+            NodeChainHeight = GetCurrentChainHeight()
+
+            If DBChainHeight = 0 Then
+                DBChainHeight = NodeChainHeight
+            End If
+
+            If GetChainTx2DB(DBChainHeight) = True Then
+                'Refresh Grid
+                Me.BeginInvoke(New EventHandler(AddressOf RefreshGrid), 1)
+            End If
+
+            'If NodeChainHeight >= DBChainHeight Then
+            '    If GetChainTx2DB(DBChainHeight) = True Then
+            '        'Refresh Grid
+            '        Me.BeginInvoke(New EventHandler(AddressOf RefreshGrid), 1)
+            '    End If
+            'End If
+        Catch ex As Exception
+            WriteErrror("Timer1", ex.Message)
+        Finally
+            Timer1.Enabled = True
+        End Try
+    End Sub
+
+
+    Private Sub RefreshGrid()
+        Dim gCnnPCE As SqlConnection
+        gCnnPCE = New SqlConnection(GetConnStr)
+
+        Try
+            gCnnPCE.Open()
+            Dim str As String
+            str = "SELECT TOP 100 * FROM TxNode ORDER BY ID DESC"
+            Dim cmd As New SqlCommand(str, gCnnPCE)
+            Dim da As New SqlDataAdapter(cmd)
+            Dim dt As New DataTable
+            da.Fill(dt)
+
+            Me.GridControl1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Exclamation)
+        Finally
+            gCnnPCE.Close()
+            gCnnPCE.Dispose()
+        End Try
+    End Sub
+
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If icount = 10 Then
+            icount = 0
+            imgGreen.SendToBack()
+        Else
+            imgGreen.BringToFront()
+        End If
+        icount = icount + 1
+    End Sub
+
+
+    Public Sub WriteErrror(ByVal ModuleName As String, ByVal ErrMsg As String)
+
+        Dim gCnnPCE As SqlConnection
+        gCnnPCE = New SqlConnection(GetConnStr)
+        gCnnPCE.Open()
+
+        Dim str As String
+        Dim cmd As New SqlCommand(str, gCnnPCE)
+        str = "INSERT INTO Errors (ModuleName, ErrMsg) VALUES (@ModuleName, @ErrMsg)"
+        cmd.CommandText = str
+        cmd.Parameters.AddWithValue("@ModuleName", ModuleName)
+        cmd.Parameters.AddWithValue("@ErrMsg", ErrMsg)
+
+        cmd.ExecuteNonQuery()
+
+        gCnnPCE.Close()
+        gCnnPCE.Dispose()
+
+    End Sub
+
+End Class
